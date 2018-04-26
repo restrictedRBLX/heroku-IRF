05269f68e4dc731b68a42584c73fa36d4b4132b1
restrictedRBLX <36389524+restrictedRBLX@users.noreply.github.com>
2018-04-26 20:24:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -53,10 +53,10 @@ def LogMessage(Moderator, Victim, Action, Reason):
     Embed.add_field(name="Punishment", value = Action, inline=False)
     return Embed
 
-def AdmissionsLog(Moderator, Victim, Action, Reason):
+def AdmissiosnLog(Moderator, Victim, Action, Reason):
     Title = Action + " by " + Moderator
     Description = Action + " for\n``` - " + Reason + "```"
-    Embed = discord.Embed(title=Title, description=Description, type="rich", color = #4C4CFF)
+    Embed = discord.Embed(title=Title, description=Description, type="rich", color = 0xFF0000)
     Embed.add_field(name="Mod", value = Moderator, inline=False)
     Embed.add_field(name="User", value = Victim, inline=False)
     Embed.add_field(name="Punishment", value = Action, inline=False)
